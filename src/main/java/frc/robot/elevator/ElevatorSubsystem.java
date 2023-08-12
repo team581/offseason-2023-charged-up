2d907c27f409f533112358d0eb238629eff9d01d
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2023-08-12 13:56:45 -0700

    Log elevator stator current instead of supply current

--- src/main/java/frc/robot/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/elevator/ElevatorSubsystem.java
@@ -85,7 +85,7 @@ public class ElevatorSubsystem extends LifecycleSubsystem {
   @Override
   public void robotPeriodic() {
     Logger.getInstance().recordOutput("Elevator/Position", getHeight());
-    Logger.getInstance().recordOutput("Elevator/Current", motor.getSupplyCurrent());
+    Logger.getInstance().recordOutput("Elevator/StatorCurrent", motor.getStatorCurrent());
     Logger.getInstance().recordOutput("Elevator/GoalPosition", goalPositionInInches);
     Logger.getInstance().recordOutput("Elevator/Homing", homingState.toString());
     Logger.getInstance().recordOutput("Elevator/AppliedVoltage", motor.getMotorOutputVoltage());

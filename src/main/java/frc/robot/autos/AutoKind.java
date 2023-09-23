4f22219322862d0fb18e9d96204c81e3fa396058
rkanemoto <73001957+rkanemoto@users.noreply.github.com>
2023-09-23 16:56:54 -0700

    Ride of "worn" autos

--- src/main/java/frc/robot/autos/AutoKind.java
+++ src/main/java/frc/robot/autos/AutoKind.java
@@ -10,23 +10,14 @@ public enum AutoKind {
   DO_NOTHING("", 0.01, 0.01, false),
 
   BLUE_LONG_SIDE_3("BlueLongSide3", 4.5, 4, false),
-  BLUE_LONG_SIDE_3_WORN("BlueLongSide3Worn", 4.5, 4, false),
-
   BLUE_MID_1_5_BALANCE("BlueMid1.5Balance", 2, 3, true),
-  BLUE_MID_1_5_BALANCE_WORN("BlueMid1.5BalanceWorn", 2, 3, true),
-
   BLUE_MID_RIGHT_2_BALANCE("BlueMidRight2Balance", 3, 3, true),
-  BLUE_MID_RIGHT_2_BALANCE_WORN("BlueMidRight2BalanceWorn", 3, 3, true),
-
   BLUE_MID_LEFT_2_BALANCE("BlueMidLeft2Balance", 3, 3, true),
-  BLUE_MID_LEFT_2_BALANCE_WORN("BlueMidLeft2BalanceWorn", 3, 3, true),
 
   BLUE_SHORT_SIDE_3("BlueShortSide3", 4.5, 4, false),
   BLUE_SHORT_SIDE_3_WORN("BlueShortSide3Worn", 4.5, 4, false),
 
   RED_MID_1_5_BALANCE("RedMid1.5Balance", 2, 3, true),
-  RED_MID_1_5_BALANCE_WORN("RedMid1.5balanceWorn", 2, 3, true),
-
   RED_MID_LEFT_2_BALANCE("RedMid2Balance", 3, 3, true),
   RED_MID_LEFT_2_BALANCE_WORN("RedMid2BalanceWorn", 3, 3, true),
 
